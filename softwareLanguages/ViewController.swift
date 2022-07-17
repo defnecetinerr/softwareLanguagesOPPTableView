4b242b24d9def24a5b1f8d1e67c9b89f2f8b21ab
Defne Cetiner <defnecetiner00@gmail.com>
2022-07-17 19:57:06 +0300

    Initial Commit

---
+++ softwareLanguages/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  softwareLanguages
+//
+//  Created by Defne Çetiner on 17.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
